1e11a1cf44da96531d7a6f50820cb784e6acd44f
lohithreddyt <lohithreddy364@gmail.com>
2026-01-02 08:37:05 +0530

    skill2

---
+++ fsadworkbook/RetailInventoryHibernateExp2/src/main/java/com/klu/util/HibernateUtil.java
@@ -0,0 +1,24 @@
+package com.klu.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory factory;
+
+    static {
+        try {
+            factory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return factory;
+    }
+}
